6cf9c176ab970c384872b3c5af84e0ad947ac669
root <root@itcast.com>
2020-10-05 19:43:50 +0800

    练习电脑维修系统3.0

--- src/main/java/com/atldy/servlet/ComputerServlet.java
+++ src/main/java/com/atldy/servlet/ComputerServlet.java
@@ -2,7 +2,7 @@ package com.atldy.servlet;
 
 import com.atldy.domain.CommonResult;
 import com.atldy.domain.ComputerAccessories;
-import com.atldy.domain.ComputerType;
+import com.atldy.domain.Coumpetrs;
 import com.atldy.service.IComputerService;
 import com.atldy.service.impl.ComputerServiceImpl;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -20,29 +20,39 @@ import java.util.Map;
 
 /**
  * @author 唐多山
- * @create 2020-09-30 11:58
+ * @create 2020-10-05 15:15
  */
-@WebServlet("/saveCcessories")
-public class SaveccessoriesServlet extends HttpServlet {
+@WebServlet("/computer")
+public class ComputerServlet extends HttpServlet {
 
     private IComputerService computerService = new ComputerServiceImpl();
 
+    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         doGet(request, response);
     }
 
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        // 判断功能
         String action = request.getParameter("action");
-        //添加电脑配件
+        //查询全部配件
+        if (action.equals("listAccessories")) {
+            List<ComputerAccessories> allAccessories = computerService.findAllAccessories();
+            //序列化json
+            ObjectMapper mapper = new ObjectMapper();
+            response.setContentType("application/json;charset=utf-8");
+            mapper.writeValue(response.getOutputStream(), allAccessories);
+        }
+
+        //添加电脑
         if (action.equals("save")) {
-            //        获取表单提交的数据
-            Map<String, String[]> parameterMap = request.getParameterMap();
-            ComputerAccessories computerAccessories = new ComputerAccessories();
             try {
-                BeanUtils.populate(computerAccessories, parameterMap);
-                System.out.println("computerAccessories = " + computerAccessories);
-                computerService.saveAccessories(computerAccessories);
+                Coumpetrs coumpetrs = new Coumpetrs();
+                Map<String, String[]> parameterMap = request.getParameterMap();
+                BeanUtils.populate(coumpetrs, parameterMap);
+                System.out.println(coumpetrs);
+                computerService.saveCoumpetrs(coumpetrs);
                 CommonResult commonResult = new CommonResult(200, "添加配件失败--->o(╥﹏╥)o");
                 ObjectMapper mapper = new ObjectMapper();
                 response.setContentType("application/json;charset=utf-8");
@@ -50,18 +60,6 @@ public class SaveccessoriesServlet extends HttpServlet {
             } catch (Exception e) {
                 throw new RuntimeException("封装数据失败了");
             }
-        }
-
-        //查询全部电脑配置
-        if (action.equals("list")) {
-            //查询全部配件
-            List<Map<String, Object>> allAccessories = computerService.findAllAccessoriesMap();
-            System.out.println("allAccessories = " + allAccessories);
-            //序列化json
-            ObjectMapper mapper = new ObjectMapper();
-            //把返回数据写到前台
-            response.setContentType("application/json;charset=utf-8");
-            mapper.writeValue(response.getOutputStream(), allAccessories);
         }
 
     }
--- src/main/resources/computer.sql
+++ src/main/resources/computer.sql
@@ -30,73 +30,21 @@ CREATE TABLE computer_accessories(
 # 创建电脑表
 DROP TABLE IF EXISTS coumpetrs;
 CREATE TABLE coumpetrs(
-  coumpetrs_id INT AUTO_INCREMENT   COMMENT '电脑编号',
+  computer_id INT AUTO_INCREMENT   COMMENT '电脑编号',
   Computer_name VARCHAR(20) DEFAULT NULL COMMENT '名称',
-  MEMORY  INT DEFAULT 0 UNIQUE COMMENT '内存',
-  cpu INT DEFAULT 0  UNIQUE COMMENT 'CPU',
-  videocard INT DEFAULT 0 UNIQUE COMMENT '显卡',
-  display INT DEFAULT 0 UNIQUE COMMENT '显示器',
-  hdd INT DEFAULT 0 UNIQUE COMMENT'硬盘',
-  price	INT DEFAULT 0  COMMENT '价格',
-  PRIMARY KEY(coumpetrs_id) 
- )ENGINE=INNODB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='computer_accessories';
- 
- # 中间表 MEMORY
- DROP  TABLE IF EXISTS computer_accessories_coumpetrs_momory;
- CREATE TABLE computer_accessories_coumpetrs_momory(
-   id INT AUTO_INCREMENT ,
-   MEMORY  INT  NOT NULL,
-   component_id INT NOT NULL,
-   PRIMARY KEY(id),
-   CONSTRAINT fk1 FOREIGN KEY(MEMORY) REFERENCES  coumpetrs(MEMORY),
-   CONSTRAINT fk2 FOREIGN KEY(component_id) REFERENCES  computer_accessories(component_id)
- )
- 
- # 中间表 cpu
- DROP  TABLE IF EXISTS computer_accessories_coumpetrs_cpu;
- CREATE TABLE computer_accessories_coumpetrs_cpu(
-   id INT AUTO_INCREMENT ,
-   cpu  INT  NOT NULL,
-   component_id INT NOT NULL,
-   PRIMARY KEY(id),
-   CONSTRAINT fk3 FOREIGN KEY(cpu) REFERENCES  coumpetrs(cpu),
-   CONSTRAINT fk4 FOREIGN KEY(component_id) REFERENCES  computer_accessories(component_id)
- )
- 
- # 中间表 videocard
- DROP  TABLE IF EXISTS computer_accessories_coumpetrs_videocard;
- CREATE TABLE computer_accessories_coumpetrs_videocard(
-   id INT AUTO_INCREMENT ,
-   videocard  INT  NOT NULL,
-   component_id INT NOT NULL,
-   PRIMARY KEY(id),
-   CONSTRAINT fk5 FOREIGN KEY(videocard) REFERENCES  coumpetrs(videocard),
-   CONSTRAINT fk6 FOREIGN KEY(component_id) REFERENCES  computer_accessories(component_id)
- )
- 
- # 中间表 display
- DROP  TABLE IF EXISTS computer_accessories_coumpetrs_display;
- CREATE TABLE computer_accessories_coumpetrs_display(
-   id INT AUTO_INCREMENT ,
-   display  INT  NOT NULL,
-   component_id INT NOT NULL,
-   PRIMARY KEY(id),
-   CONSTRAINT fk7 FOREIGN KEY(display) REFERENCES  coumpetrs(display),
-   CONSTRAINT fk8 FOREIGN KEY(component_id) REFERENCES  computer_accessories(component_id)
- )
- 
- # 中间表 display
- DROP  TABLE IF EXISTS computer_accessories_coumpetrs_hdd;
- CREATE TABLE computer_accessories_coumpetrs_hdd(
-   id INT AUTO_INCREMENT ,
-   hdd  INT  NOT NULL,
-   component_id INT NOT NULL,
-   PRIMARY KEY(id),
-   CONSTRAINT fk9 FOREIGN KEY(hdd) REFERENCES  coumpetrs(hdd),
-   CONSTRAINT fk10 FOREIGN KEY(component_id) REFERENCES  computer_accessories(component_id)
- )
- 
- 
+  MEMORY  INT DEFAULT 0 COMMENT '内存',
+  cpu INT DEFAULT 0 COMMENT 'CPU',
+  videocard INT DEFAULT 0 COMMENT '显卡',
+  display INT DEFAULT 0 COMMENT '显示器',
+  hdd INT DEFAULT 0 COMMENT'硬盘',
+  price	INT DEFAULT 0 COMMENT '价格',
+  PRIMARY KEY(computer_id)  ,	
+  CONSTRAINT fk1_coumpetrs FOREIGN KEY(MEMORY) REFERENCES  computer_accessories(component_id) ,
+  CONSTRAINT fk2_coumpetrs FOREIGN KEY(cpu) REFERENCES  computer_accessories(component_id)	,
+  CONSTRAINT fk3_coumpetrs FOREIGN KEY(videocard) REFERENCES  computer_accessories(component_id),
+  CONSTRAINT fk4_coumpetrs FOREIGN KEY(display) REFERENCES  computer_accessories(component_id),
+  CONSTRAINT fk5_coumpetrs FOREIGN KEY(hdd) REFERENCES  computer_accessories(component_id)			
+)ENGINE=INNODB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='computer_accessories';
 
 
 INSERT INTO computer_type(typename) VALUES('cpu');
@@ -110,19 +58,22 @@ INSERT INTO computer_accessories(Type_id,Component_name,capacity,price) VALUES(2
 INSERT INTO computer_accessories(Type_id,Component_name,capacity,price) VALUES(1,'AMDi7-1580U',DEFAULT,954);
 INSERT INTO computer_accessories(Type_id,Component_name,capacity,price) VALUES(3,'金士顿内存','8',565);
 INSERT INTO computer_accessories(Type_id,Component_name,capacity,price) VALUES(4,'三星GTX3.5显卡',DEFAULT,399);
-INSERT INTO computer_accessories(Type_id,Component_name,capacity,price) VALUES(2,'三星孤星17寸显示器',DEFAULT,666);	
+INSERT INTO computer_accessories(Type_id,Component_name,capacity,price) VALUES(5,'三星孤星17寸显示器',DEFAULT,666);	
 SELECT * FROM computer_accessories;
-SELECT computer_accessories.* FROM computer_accessories,computer_type WHERE  computer_type.Type_id=computer_accessories.`Type_id` AND typename='cpu'
+SELECT  computer_accessories.*,computer_type.`typename` FROM computer_accessories,computer_type WHERE  computer_type.Type_id=computer_accessories.`Type_id` 
+
 
 INSERT INTO coumpetrs 
 	(Computer_name,MEMORY,cpu,videocard,display,hdd,price)
 VALUES ('冲锋战神游戏本',3,2,4,4,1,5000);
+
 INSERT INTO coumpetrs 
 	(Computer_name,MEMORY,cpu,videocard,display,hdd,price)
-VALUES ('神职者笔记本',3,5,4,2,1,5000);		
+VALUES ('神职者笔记本',3,5,4,2,1,5000);	
+	
 SELECT * FROM 	coumpetrs;
 
-SELECT computer_accessories.* FROM computer_accessories,coumpetrs WHERE computer_accessories.`component_id`=coumpetrs.`cpu`  
+SELECT computer_accessories.* FROM computer_accessories,coumpetrs WHERE computer_accessories.`component_id`=coumpetrs.`memory`  
 
 
 
